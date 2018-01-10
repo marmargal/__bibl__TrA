c01ee0bdd137fa508c5238c0fdc9fa5b9f78d03a
Juan Carlos Cansino <jucansu@gmail.com>
2018-01-10 13:38:47 +0100

    creacion usuarios

--- Biblioteca/Biblioteca/urls.py
+++ Biblioteca/Biblioteca/urls.py
@@ -14,4 +14,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    
+    #nuevoUsuario
+    url(r'^usuario/nuevo$','principal.views.nuevo_usuario'),
 )
